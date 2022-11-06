155192fa4a8e029e3da2bc89f76202c785eab1f9
7code <7code@mxt-c.com>
2022-11-06 15:30:27 +0100

    Dummy data added

---
+++ migrations/002-dummy-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Post
+    (title, content, createdAt)
+values
+    ("Post 1", "Sample post content 1", "2022-11-04T14:20:44.358Z");
+
+INSERT INTO Post
+    (title, content, createdAt)
+values
+    ("Post 2", "Sample post content 2", "2022-11-05T14:20:44.358Z");
