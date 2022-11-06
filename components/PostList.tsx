00040a6cd89a6ebd57203717f9d77d4c406a752c
7code <7code@mxt-c.com>
2022-11-07 00:06:21 +0100

    Added props to components

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,32 +1,12 @@
+import { Post } from "@/src/types/post";
 import React from "react";
 import PostListItem from "./PostListItem";
 
-const PostList: React.FC = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "test 1",
-      content: "test content 1",
-      createdAt: "2022-11-04T14:20:44.358Z",
-    },
-    {
-      id: 2,
-      title: "test 2",
-      content: "test content 2",
-      createdAt: "2022-11-04T14:20:44.358Z",
-    },
-    {
-      id: 3,
-      title: "test 3",
-      content: "test content 3",
-      createdAt: "2022-11-04T14:20:44.358Z",
-    },
-  ];
-
+const PostList: React.FC<{ posts: Post[] }> = ({ posts }) => {
   return (
     <>
       {posts.map((item) => (
-        <PostListItem key={item.id} />
+        <PostListItem {...item} key={item.id} />
       ))}
     </>
   );
--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Link from "next/link";
+import { Post } from "@/src/types/post";
 
-const PostListItem: React.FC = () => {
+const PostListItem: React.FC<Post> = ({ id, title, createdAt }) => {
   return (
     <div className="card mb-2">
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <a href="#" className="card-link">
+        <h5 className="card-title">{title}</h5>
+        <p>{createdAt}</p>
+        <Link className="card-link" href={`/posts/${id}`}>
           Read more...
-        </a>
+        </Link>
       </div>
     </div>
   );
