32098ec7f6679da9ecc6f60437fcbc8fde127f01
7code <7code@mxt-c.com>
2022-11-07 23:19:04 +0100

    Create post e2e test added

--- cypress/e2e/post-create.cy.js
+++ cypress/e2e/post-create.cy.js
@@ -21,10 +21,34 @@ describe("Create Post", () => {
     cy.get("button[type=submit]").should("be.enabled");
   });
 
-  it("should not be available to save post without authorization", (done) => {
+  it("should not be available to save post without authorization", () => {
     cy.get("input[name='title']").type("my new post");
     cy.get("textarea[name='content']").type("my post content");
     cy.get("button[type=submit]").click();
     cy.get("input[name='title']").should("have.attr", "value", "my new post");
+  });
+
+  it("should save post with authorization", () => {
+    const postTitle = "my new post";
+    const random = (Math.random() * 1000).toString(16).substring(0, 6);
+
+    cy.get('a[href*="/login"]').click();
+
+    cy.get("input[name='pin']").type("1234");
+
+    cy.get("button[type=submit]").click();
+
+    cy.wait(2000);
+
+    cy.get("input[name='title']").type(`${postTitle} ${random}`);
+    cy.get("textarea[name='content']").type("my post content");
+    cy.get("button[type=submit]").click();
+
+    cy.wait(2000);
+
+    cy.get(".card")
+      .last()
+      .get(".card-title")
+      .contains(`${postTitle} ${random}`);
   });
 });
