4e00937db8ecb67075971baad7feeeaaea5de513
7code <7code@mxt-c.com>
2022-11-08 13:58:04 +0100

    migration script updated

--- scripts/db-migration.js
+++ scripts/db-migration.js
@@ -2,11 +2,17 @@ const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
 const setup = async () => {
-  const db = await open({
-    filename: "./db.sqlite",
-    driver: sqlite3.Database,
-  });
-  await db.migrate({ force: true });
+  console.log("Migration: Start..");
+  try {
+    const db = await open({
+      filename: "./db.sqlite",
+      driver: sqlite3.Database,
+    });
+    await db.migrate({ force: true });
+    console.log("Migration: Success!");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 setup();
